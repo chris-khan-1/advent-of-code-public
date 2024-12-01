81debd668983777ee8a8392b2044baa2ac74b9bd
chris_khan <ckhan46.ck@gmail.com>
2024-12-01 12:04:11 +0000

    copy

--- src/aoc_2024/day_01/aoc_2024_01_solution.py
+++ src/aoc_2024/day_01/aoc_2024_01_solution.py
@@ -1,3 +1,31 @@
+def parse_input(input: list[str]) -> tuple[list, list]:
+    left = []
+    right = []
+    for i, value in enumerate(input):
+        left_value, right_value = value.split()
+        left.append(int(left_value))
+        right.append(int(right_value))
+    return sorted(left), sorted(right)
+
+
+def get_difference(a: int, b: int) -> int:
+    return abs(a - b)
+
+
+def get_total_difference(left: list, right: list) -> int:
+    total = 0
+    for i in range(len(left)):
+        total += get_difference(a=left[i], b=right[i])
+    return total
+
+
+def get_total_similarity(left: list, right: list) -> int:
+    total = 0
+    for i in set(left):
+        total += i * left.count(i) * right.count(i)
+    return total
+
+
 if __name__ == "__main__":
     import sys
     from pathlib import Path
@@ -9,11 +37,15 @@ if __name__ == "__main__":
     input_path = "./src/aoc_2024/day_01/input.txt"
     result_path = "./src/aoc_2024/day_01/result.txt"
 
-    string_input = helpers.read_file(file_path=input_path)
+    input = helpers.read_file_lines(file_path=input_path)
+    left_input, right_input = parse_input(input=input)
+
+    total_distance = get_total_difference(left=left_input, right=right_input)
+    total_similarity = get_total_similarity(left=left_input, right=right_input)
 
     result = f"""\
-        Part 1: {1}
-        Part 2: {2}
+        Part 1: The total distance is {total_distance}.
+        Part 2: The total similarity is {total_similarity}.
         """
 
     helpers.write_file(file_path=result_path, contents=result)
--- tests/aoc_2024/day_01/test_aoc_2024_01_solution.py
+++ tests/aoc_2024/day_01/test_aoc_2024_01_solution.py
@@ -1,2 +1,53 @@
-# import pytest
-# from src.aoc_2024.day_01.aoc_2024_01_solution import *
+import pytest
+
+from src.aoc_2024.day_01.aoc_2024_01_solution import (
+    get_difference,
+    get_total_difference,
+    get_total_similarity,
+    parse_input,
+)
+
+
+def test_parse_input():
+    input = [
+        "3   4",
+        "4   3",
+        "2   5",
+        "1   3",
+        "3   9",
+        "3   3",
+    ]
+    actual_one, actual_two = parse_input(input=input)
+
+    assert actual_one == [1, 2, 3, 3, 3, 4]
+    assert actual_two == [3, 3, 3, 4, 5, 9]
+
+
+@pytest.mark.parametrize(
+    argnames=["a", "b", "expected"],
+    argvalues=[
+        pytest.param(3, 7, 4, id="right bigger"),
+        pytest.param(9, 3, 6, id="left bigger"),
+    ],
+)
+def test_get_difference(a, b, expected):
+    actual = get_difference(a=a, b=b)
+    assert actual == expected
+
+
+def test_get_total_difference():
+    left_input = [1, 2, 3, 3, 3, 4]
+    right_input = [3, 3, 3, 4, 5, 9]
+
+    actual = get_total_difference(left=left_input, right=right_input)
+
+    assert actual == 11
+
+
+def test_get_total_similarity():
+    left_input = [1, 2, 3, 3, 3, 4]
+    right_input = [3, 3, 3, 4, 5, 9]
+
+    actual = get_total_similarity(left=left_input, right=right_input)
+
+    assert actual == 31
